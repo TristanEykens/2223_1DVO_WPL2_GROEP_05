f3b84dc12598853f9944c48b151d834d14b7b234
simonlambie <simon.lambie@student.pxl.be>
2023-05-23 10:50:35 +0200

    laatste change webshop

--- webshoptest.js
+++ webshoptest.js
@@ -1,5 +1,5 @@
 function incrementValue(){
-    var waarde = parseInt(document.getElementById('number').value, 10);
+    var waarde = parseInt(document.getElementById("number").value, 10);
     waarde = isNaN(waarde) ? 0 : waarde;
     waarde++;
     document.getElementById('number').value = waarde;
@@ -12,4 +12,15 @@ function  decrementValue(){
     waarde--;
     }
     document.getElementById('number').value = waarde;
+}
+
+function addToCart (){
+    var input = document.getElementById('number');
+    var winkelmandhoeveelheid = parseInt(document.getElementById('winkelmand-hoeveelheid').textContent);
+    var waarde = parseInt((input.value, 10));
+    winkelmandhoeveelheid = isNaN(winkelmandhoeveelheid) ? 0 : winkelmandhoeveelheid;
+    waarde = isNaN(waarde) ? 0 : waarde;
+    winkelmandhoeveelheid += waarde;
+    document.getElementById('winkelmand-hoeveelheid').textContent = winkelmandhoeveelheid;
+
 }
